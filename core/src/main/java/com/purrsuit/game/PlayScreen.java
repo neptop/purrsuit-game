110d45198e53ed785737954405d0e6dfca545b92
neptop <davidhoang777@gmail.com>
2025-09-26 15:19:56 -0700

    Refactor package structure and added PlayScreen
    Moved the lwjgl3 launcher and the helper classes to the com.purrsuit.desktop package. Thenb added a new PlayScreen class as the main game screen. Upated build.gradle to use the renamed main class.

---
+++ core/src/main/java/com/purrsuit/game/PlayScreen.java
@@ -0,0 +1,15 @@
+package com.purrsuit.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class PlayScreen extends ScreenAdapter {
+    private final OrthographicCamera cam = new OrthographicCamera();
+    private final FitViewport viewport = new FitViewport(28,31, cam); // tile units
+
+    @Override
+    public void render (float delta) {
+        // game logic and rendering code will go here later
+    }
+}
