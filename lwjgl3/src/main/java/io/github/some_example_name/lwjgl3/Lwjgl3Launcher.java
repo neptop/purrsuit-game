6cec2304db386135e3f369c1ba9b9f68ddf48ff8
neptop <davidhoang777@gmail.com>
2025-09-26 14:43:49 -0700

    Refactor to use PurrsuitGame as main entry point
    Replaces Main with PurrsuitGame as the main application class. Then updated package names in build.gradle and Lwjgl3Launcher to follow the updated structure

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Lwjgl3Launcher.java
@@ -2,7 +2,7 @@ package io.github.some_example_name.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.github.some_example_name.Main;
+import com.purrsuit.game.PurrsuitGame;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
@@ -12,12 +12,12 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication() {
-        return new Lwjgl3Application(new Main(), getDefaultConfiguration());
+        return new Lwjgl3Application(new PurrsuitGame(), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
         Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
-        configuration.setTitle("YourProjectName");
+        configuration.setTitle("Purrsuit");
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         configuration.useVsync(true);
