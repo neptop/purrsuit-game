abaead7e3844a606d7d3ecd0eb0782320eb1175e
neptop <davidhoang777@gmail.com>
2025-09-27 13:19:24 -0700

    Remove unused PPM from GameConfig

--- core/src/main/java/com/purrsuit/game/util/GameConfig.java
+++ core/src/main/java/com/purrsuit/game/util/GameConfig.java
@@ -4,7 +4,6 @@ public final class GameConfig {
     private GameConfig() {}
     public static final int VIEW_W_TILES = 28; //
     public static final int VIEW_H_TILES = 31; //
-    public static final float PPM = 16f; // pixels per meter (for Box2D scaling)
     public static final float WORLD_WIDTH = VIEW_W_TILES;
     public static final float WORLD_HEIGHT = VIEW_H_TILES;
 }
